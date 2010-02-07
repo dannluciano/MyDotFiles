6663494e78681888b9e4c32e38e3d498543beb3b
Dann Luciano <dannluciano@gmail.com>
2010-02-07 11:19:49 -0300

    Primeiro Commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+cp bash_completion.sh ~/.bash_completion.sh
+cp bash_profile ~/.bash_profile
+cp colors.sh ~/.colors.sh
+cp git_completion.sh ~/.git_completion.sh
+cp gitconfig ~/.gitconfig
+cp irbc ~/.irbc
+cp rake_completion.rb ~/.rake_completion.rb
+cp gemrc ~/.gemrc
+cp gitignore ~/.gitignore
+
+chmod 755 ~/.rake_completion.rb
+
+sudo mkdir -p /etc/bash_completion
+
+source ~/.bash_profile
