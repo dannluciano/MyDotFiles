ea71207301a3b37045dfbdb9fe1c84533fe53e12
Dann Luciano <dannluciano@gmail.com>
2011-06-24 18:57:24 -0300

    Update install sh

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf ~/.emacs.d
-cp -R ./ ~/.emacs.d
+cp bash_completion.sh ~/.bash_completion.sh
+cp bash_profile ~/.bash_profile
+cp colors.sh ~/.colors.sh
+cp gem_completion.sh ~/.gem_completion.sh
+cp gemrc ~/.gemrc
+cp git_completion.sh ~/.git_completion.sh
+cp gitattributes ~/.gitattributes
+cp gitconfig ~/.gitconfig
+cp gitignore ~/.gitignore
+cp emacs.d ~/.emacs.d
+ 
+cp irbrc ~/.irbrc
+cp rake_completion.rb ~/.rake_completion.rb
+ 
+chmod 755 ~/.rake_completion.rb
+ 
+sudo mkdir -p /etc/bash_completion
+ 
+source ~/.bash_profile
